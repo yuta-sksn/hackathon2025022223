f5b573a557c1e4d61ee4171b9b5d2e6943d32e41
Yuta Sukekawa <sukekawa@thymefield.jp>
2025-02-22 16:21:54 +0900

    delete: 不要なファイルを削除

--- src/features/university/api/getUniversity.ts
+++
@@ -1,6 +0,0 @@
-import fetcher from "@/utils/fetcher"
-import { UniversityResponse, University } from "../types"
-
-export const getUniversity = async (uuid: string) => {
- return fetcher<UniversityResponse, University>(`/v1/universities/${uuid}`)
-}
